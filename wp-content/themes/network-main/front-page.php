65af8f0acba14ad0cf1e4a20eb6f6377a867cf74
Pea <pea@misfist.com>
2015-07-22 01:14:13 -0400

    Removed bxslider JS from page template.

--- wp-content/themes/network-main/front-page.php
+++ wp-content/themes/network-main/front-page.php
@@ -30,30 +30,6 @@
 		</section>
 
 		<section class="home-modules">
-			
-			<script type="text/javascript">
-			jQuery(document).ready(function(){
-				jQuery('.widget_glocal_network_posts_widget .network-posts-list').bxSlider({
-					slideWidth: 5000,
-					minSlides: 2,
-					maxSlides: 2,
-					slideMargin: 10,
-					pager: false
-				});
-				var responsive_viewport = jQuery(window).width();
-				if (responsive_viewport < 320) {
-					jQuery('.widget_glocal_network_posts_widget .network-posts-list').reloadSlider({
-					slideWidth: 5000,
-					minSlides: 1,
-					maxSlides: 1,
-					slideMargin: 10,
-					pager: false
-					});
-				} 
-			});
-			</script>
-
-
 			<?php // Widgets
 
 			dynamic_sidebar( 'home-modules' ); ?>
