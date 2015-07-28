fca28eb8a9a80547eb1e1ebc58a8ae0a3d731ec0
Pea <pea@misfist.com>
2015-07-28 11:58:29 -0400

    Add new branch for global menu.

---
+++ wp-content/plugins/anp-global-menu/anp-global-menu-render.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * 
+ * Register Scripts and Localize Data to Scripts
+ * @return array of key / value pairs and localize to registered script
+ * 
+ **/
+
+function anp_global_menu_register_scripts() {
+
+
+    if ( !is_admin() ) {
+         wp_register_script( 'anp-global-menu-js', ANP_GLOBAL_MENU_URL . '/assets/js/anpGlobalMenu.js', array('jquery'), null, true );
+    }
+
+    // Retrieve menu name stored in wp_sitemeta - don't think this is needed.
+    $options = get_site_option( 'anp-global-nav-menu' );
+
+    // Get menu data
+    $anp_global_menu = anp_global_menu_get_main_menu();
+
+    if( $anp_global_menu ) {
+
+        // Assign values to array that will be made available for jQuery
+        $menuData = array(
+            'status'            => 'OK',
+            'networkURL'        => get_blog_details(1)->siteurl,
+            'networkName'       => get_blog_details(1)->blogname,
+            'networkLogo'       => '<a href="' . get_blog_details(1)->siteurl . '">' . get_blog_details(1)->blogname . '</a>',
+            'globalMenu'        => $anp_global_menu,
+        );
+
+    } else {
+
+        // Send error info
+        $menuData = array(
+            'status'            => 'Error',
+            'errorCode'         => 'No Menu',
+            'errorMessage'      => 'No menu was returned',
+        );
+
+    }
+
+    // Make WP variables array available globally available
+    // https://codex.wordpress.org/Function_Reference/wp_localize_script
+    wp_localize_script( 'anp-global-menu-js', 'anpGlobalMenuVars', $menuData );
+
+    // Only add global nav to sub-sites
+    if( !is_main_site() ) {
+
+        wp_enqueue_script( 'anp-global-menu-js' );
+
+    } else {
+
+        // Error
+
+    }
+
+    $anp_global_menu_css = ANP_GLOBAL_MENU_URL . '/assets/css/style.css';
+
+    // Provide filter to override style
+    $anp_global_menu_css = apply_filters( 'anp_global_menu_css_override', $anp_global_menu_css );
+    
+    // Enqueue styles
+    wp_enqueue_style( 'anp_global_menu_stylesheet', $anp_global_menu_css);
+    
+}
+
+add_action( 'wp_enqueue_scripts', 'anp_global_menu_register_scripts' );
+
+
+/**
+ *
+ * Get Global Menu
+ * @return string of menu markup or null
+ * @param integer value for echo - 0 or 1
+ *
+ **/
+
+function anp_global_menu_get_main_menu( $echo_menu = 0 ) {
+
+    $main_site_id = 1;
+
+    if ( false === ( $main_menu = get_site_transient( 'anp_global_menu_markup' ) ) ) {
+    // If there is no valid transient set
+
+        // Get anp-global-menu options table from the main site
+        $anp_main_menu = get_site_option( 'anp-global-nav-menu' );
+
+        // SWITCH TO MAIN SITE TABLE
+        switch_to_blog( $main_site_id );
+
+        if( $anp_main_menu ) {
+
+            // Fetch the menu and assign to $main_menu
+            $main_menu = wp_nav_menu( array(
+                'container'         => false,                           // remove nav container
+                'menu'              => $anp_main_menu,                  // nav name
+                'menu_class'        => 'global-menu menu clearfix',     // adding custom nav class
+                'menu_id'           => '',                              // menu id
+                'depth'             => 0,                               // limit the depth of the nav
+                'fallback_cb'       => 'false',                         // fallback function - FALSE
+                'echo'              => $echo_menu
+            ) );
+
+            // RESTORE! - SWITCH BACK TO CURRENT SITE TABLE
+            restore_current_blog();
+
+        }
+
+        set_site_transient( 'anp_global_menu_markup', $main_menu, 28800 );
+
+    }
+
+    return $main_menu;
+
+}
+
+
+/**
+ *
+ * Template Tag
+ * Menu to template
+ * @return echo the global menu
+ *
+ **/
+
+function anp_show_global_menu() {
+
+    echo anp_global_menu_get_main_menu();
+
+}
+
+?>
---
+++ wp-content/plugins/anp-global-menu/class-anp-global-menu-options.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * The file that defines the network settings options class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       http://example.com
+ * @since      0.0.1-dev
+ *
+ * @package    ANP_Global_Menu
+ * @subpackage ANP_Global_Menu/classes
+ */
+
+class ANP_Global_Menu_Options {
+ 
+    private static $instance;
+    /**
+     * Get active object instance
+     *
+     * @since 1.0
+     *
+     * @access public
+     * @static
+     * @return object
+     */
+    public static function get_instance() {
+ 
+        if ( ! self::$instance )
+            self::$instance = new ANP_Global_Menu_Options();
+ 
+        return self::$instance;
+    }
+ 
+    /**
+     * Class constructor.  Includes constants, includes and init method.
+     *
+     * @since 1.0
+     *
+     * @access public
+     * @return void
+     */
+    public function __construct() {
+        $this->init();
+    }
+ 
+    /**
+     * Run action and filter hooks.
+     *
+     * @since 1.0
+     *
+     * @access private
+     * @return void
+     */
+    private function init() {
+ 
+        //Adds settings to Network Settings
+        add_filter( 'wpmu_options'       , array( $this, 'show_network_settings' ) );
+        add_action( 'update_wpmu_options', array( $this, 'save_network_settings' ) );
+ 
+    }
+ 
+    public static function save_network_settings() {
+        $posted_settings  = array_map( 'sanitize_text_field', $_POST['anp-global-menu'] );
+ 
+        foreach ( $posted_settings as $name => $value ) {
+            update_site_option( $name, $value );
+        }
+    }
+ 
+    public static function show_network_settings() {
+        $settings = self::get_network_settings();
+    ?>
+        <h3><?php _e( 'Activist Network Global Menu' ); ?></h3>
+        <table id="menu" class="form-table">
+
+        <?php
+            foreach ( $settings as $setting ) :
+                ?>
+     
+                <tr valign="top">
+                    <th scope="row"><?php echo $setting['name']; ?></th>
+                    <td>
+
+                        <?php if( 'select' == $setting['type'] ) { ?>
+
+                            <?php// var_dump( $setting['options'] ) ?>
+
+                            <?php $selected_menu = get_site_option( $setting['id'] ); ?>
+
+                            <select name="anp-global-menu[<?php echo $setting['id']; ?>]" id="anp-global-menu">
+
+                                <option value='' <?php selected( $selected_menu, '' ); ?>>--Select a Menu--</option>
+
+                                <?php $options = $setting['options'];
+
+                                foreach( $options as $option ) { ?>
+
+                                    <option value="<?php echo $option->slug; ?>"  <?php selected( $selected_menu, $option->slug ); ?>><?php echo $option->name; ?></option>
+
+                                <?php } ?>
+
+                            </select>    
+
+                        <?php } elseif ( 'text' == $setting['type'] ) { ?>
+                            
+                            <input type="<?php echo $setting['type'];?>" name="anp-global-menu[<?php echo $setting['id']; ?>]" value="<?php echo esc_attr( get_site_option( $setting['id'] ) ); ?>" />
+                            <br /><?php echo $setting['desc']; ?>
+
+                        <?php } ?>
+
+                    </td>
+                </tr>
+
+                <?php
+
+            endforeach;
+
+        echo '</table>';
+
+    }
+ 
+    public static function get_network_settings() {
+ 
+        $settings[] = array(
+                    'id'            => 'anp-global-nav-menu',
+                    'name'          => __( 'Select a Menu' ),
+                    'desc'          => __( 'Please select a menu to use as the global network menu.' ),
+                    'options'       => get_terms( 'nav_menu', array( 'hide_empty' => true ) ), // Must be an array
+                    'type'          => 'select'
+        );
+ 
+        return apply_filters( 'plugin_settings', $settings );
+    }
+
+}
+ 
