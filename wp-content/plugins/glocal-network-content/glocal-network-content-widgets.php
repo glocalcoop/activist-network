46e404c815cb98b84bc3d2b327841b121e21a68e
Pea <pea@misfist.com>
2015-08-17 22:36:12 -0400

    Fixed excerpt bug.

--- wp-content/plugins/glocal-network-content/glocal-network-content-widgets.php
+++ wp-content/plugins/glocal-network-content/glocal-network-content-widgets.php
@@ -255,6 +255,7 @@ class ANP_Network_Posts_Widget extends WP_Widget {
             unset( $instance['include_categories'] );
         }
 
+
         //TODO
         // Make sure ID & Class fields contain valid characters
 
@@ -271,6 +272,10 @@ class ANP_Network_Posts_Widget extends WP_Widget {
         }
 
         echo $after_widget;
+
+        echo '<pre>';
+        var_dump( $instance );
+        echo '</pre>';
 
     }
 
@@ -289,7 +294,7 @@ class ANP_Network_Posts_Widget extends WP_Widget {
             'show_meta' => true,
             'show_thumbnail' => false,
             'show_excerpt' => true,
-            'excerpt_length' => '',
+            'excerpt_length' => 20,
             'show_site_name' => true,
         ) );
 
@@ -520,7 +525,7 @@ class ANP_Network_Post_Highlights_Widget extends WP_Widget {
             'show_meta' => true,
             'show_thumbnail' => false,
             'show_excerpt' => true,
-            'excerpt_length' => '',
+            'excerpt_length' => 20,
             'show_site_name' => true,
         ) );
 
