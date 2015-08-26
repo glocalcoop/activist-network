072b1ddc559b1766b7705a22cdc821fc8febdcbc
Pea <pea@misfist.com>
2015-08-26 13:54:05 -0400

    Updated styles for global menu plugin to match main site theme and be consistent across themes.

--- wp-content/plugins/anp-global-menu/gulpfile.js
+++ wp-content/plugins/anp-global-menu/gulpfile.js
@@ -1,58 +1,73 @@
-// npm install --save-dev gulp gulp-plumber gulp-watch gulp-livereload gulp-minify-css gulp-jshint jshint-stylish gulp-uglify gulp-rename gulp-notify gulp-include gulp-sass
+// npm install --save-dev gulp gulp-plumber gulp-watch gulp-livereload gulp-concat gulp-minify-css gulp-jshint jshint-stylish gulp-uglify gulp-rename gulp-notify gulp-include gulp-sass
 
 var gulp = require('gulp'),
-	autoprefixer = require('gulp-autoprefixer'),
 	plumber = require( 'gulp-plumber' ),
 	watch = require( 'gulp-watch' ),
 	livereload = require( 'gulp-livereload' ),
 	minifycss = require( 'gulp-minify-css' ),
-	jshint = require( 'gulp-jshint' ),
-	stylish = require( 'jshint-stylish' ),
+	// jshint = require( 'gulp-jshint' ),
+	// stylish = require( 'jshint-stylish' ),
 	uglify = require( 'gulp-uglify' ),
 	rename = require( 'gulp-rename' ),
 	notify = require( 'gulp-notify' ),
 	include = require( 'gulp-include' ),
 	sass = require( 'gulp-sass' );
 
-var onError = function( err ) {
-	console.log( 'An error occurred:', err.message );
+var onError = function( error ) {
+	console.error( error.message );
 	this.emit( 'end' );
 }
 
-gulp.task( 'sass', function() {
+// Styles
+gulp.task( 'styles', function() {
 	return gulp.src( './assets/src/sass/style.scss', {
-		style: 'expanded'
+		sourceComments: 'map',
+		sourceMap: 'scss',
+		outputStyle: 'nested'
 	} )
 	.pipe( plumber( { errorHandler: onError } ) )
 	.pipe( sass() )
-	//.pipe( autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4') )
 	.pipe( gulp.dest( './assets/css' ) )
 	.pipe( minifycss() )
 	.pipe( rename( { suffix: '.min' } ) )
 	.pipe( gulp.dest( './assets/css' ) )
-	.pipe( livereload() );
+	.pipe( livereload() )
+	.pipe( notify({ message: 'Styles task complete' }));
 } );
 
 // Scripts
-gulp.task('scripts', function() {
-  return gulp.src( 'assets/js-src/**/*.js' )
-    .pipe( gulp.dest( 'assets/js' ) )
-    .pipe( rename( { suffix: '.min' } ) )
-    .pipe( uglify() )
-    .pipe( gulp.dest('assets/js') )
-    .pipe( notify( { message: 'Scripts task complete' } ) )
-    .pipe( livereload() );
+// gulp.task('scripts', function() {
+//   return gulp.src( 'src/js/**/*.js' )
+//     .pipe( concat( 'main.js') )
+//     .pipe( gulp.dest( 'assets/js' ) )
+//     .pipe( rename( { suffix: '.min' } ) )
+//     .pipe( uglify() )
+//     .pipe( gulp.dest('assets/js') )
+//     .pipe( notify( { message: 'Scripts task complete' } ) );
+// });
+
+
+// Watch
+gulp.task('watch', function() {
+
+	// Watch .scss files
+	gulp.watch('assets/src/sass/**/*.scss', ['styles']);
+
+	// Watch .js files
+	//gulp.watch('library/js/**/*.js', ['scripts']);
+
+	gulp.watch( './**/*.php' ).on( 'change', function( file ) {
+		livereload.changed( file );
+	} );
+
+	// Create LiveReload server
+	livereload.listen();
+
+	// Watch any files in dist/, reload on change
+	gulp.watch(['assets/**']).on('change', livereload.changed);
+
 });
 
-gulp.task( 'watch', function() {
-  livereload.listen();
-  gulp.watch( './assets/sass/**/*.scss', [ 'sass' ] );
-  gulp.watch( './assets/js/**/*.js', [ 'scripts' ] );
-  gulp.watch( './**/*.php' ).on( 'change', function( file ) {
-    livereload.changed( file );
-  } );
+gulp.task( 'default', [ 'styles', 'watch' ], function() {
+
 } );
-
-gulp.task( 'default', [ 'sass', 'scripts', 'watch' ], function() {
-
-} )
