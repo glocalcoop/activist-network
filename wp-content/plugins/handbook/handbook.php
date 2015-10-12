e60fe798038a7fd172fa6a72ad02b60dba92518a
Dana Skallman <dana@glocal.coop>
2015-10-12 14:44:57 -0400

    Plugin - Handbook from WPorg

---
+++ wp-content/plugins/handbook/handbook.php
@@ -0,0 +1,238 @@
+<?php
+/**
+ * Plugin Name: Handbook
+ * Description: Features for a handbook, complete with glossary and table of contents
+ * Author: Nacin
+ */
+
+require_once dirname( __FILE__ ) . '/inc/callout-boxes.php';
+require_once dirname( __FILE__ ) . '/inc/glossary.php';
+require_once dirname( __FILE__ ) . '/inc/table-of-contents.php';
+require_once dirname( __FILE__ ) . '/inc/email-post-changes.php';
+require_once dirname( __FILE__ ) . '/inc/watchlist.php';
+
+WPorg_Handbook_Glossary::init();
+
+/**
+ * Initialize our handbooks
+ *
+ */
+class WPorg_Handbook_Init {
+
+	static function init() {
+
+		$post_types = (array) apply_filters( 'handbook_post_types', array( 'handbook' ) );
+
+		new WPorg_Handbook_TOC( $post_types );
+
+		foreach ( $post_types as $type ) {
+			new WPorg_Handbook( $type );
+		}
+
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_styles' ) );
+	}
+
+	static public function enqueue_styles() {
+		wp_enqueue_style( 'wporg-handbook-css', plugins_url( '/stylesheets/callout-boxes.css', __FILE__ ), array(), '20150507' );
+	}
+
+}
+
+add_action( 'after_setup_theme', array( 'WPorg_Handbook_Init', 'init' ) );
+
+class WPorg_Handbook {
+
+	public $post_type = '';
+	public $setting_name = '';
+
+	protected $label = '';
+
+	static function caps() {
+		return array(
+			'edit_handbook_pages', 'edit_others_handbook_pages',
+			'edit_published_handbook_pages',
+		);
+	}
+
+	static function editor_caps() {
+		return array(
+			'publish_handbook_pages',
+			'delete_handbook_pages', 'delete_others_handbook_pages',
+			'delete_published_handbook_pages', 'delete_private_handbook_pages',
+			'edit_private_handbook_pages', 'read_private_handbook_pages',
+		);
+	}
+
+	/**
+	 * Returns the handbook name.
+	 *
+	 * If one isn't set via settings, one is generated.
+	 *
+	 * @param  string $post_type Optional. Handbook post type.
+	 * @param  bool   $raw       Optional. Return only explicitly set name without attempting to generate default name?
+	 * @return string
+	 */
+	static function get_name( $post_type = 'handbook', $raw = false ) {
+		// Prefer explicitly configured handbook name.
+		$name = get_option( $post_type . '_name' );
+
+		// If handbook name isn't set, try root relative site path.
+		if ( ! $raw && empty( $name ) ) {
+			if ( is_multisite() ) {
+				$name = trim( get_blog_details()->path, '/' );
+			} else {
+				$name = trim( parse_url( get_option( 'home' ), PHP_URL_PATH ), '/' );
+			}
+
+			// If no name defined yet, try handbook post type if not standard.
+			if ( empty( $name ) && ( 'handbook' != $post_type ) ) {
+				$name = ucfirst( substr( $post_type, 0, -9 ) );
+			}
+
+			$name .= ' Handbook';
+		}
+
+		return trim( $name );
+	}
+
+	function __construct( $type ) {
+		if ( 'handbook' != $type ) {
+			$this->post_type = $type . '-handbook';
+		} else {
+			$this->post_type = $type;
+		}
+
+		$this->label = ucwords( str_replace( array( '-', '_' ), ' ', $this->post_type ) );
+
+		$this->setting_name = $this->post_type . '_name';
+
+		add_filter( 'user_has_cap',                       array( $this, 'grant_handbook_caps' ) );
+		add_filter( 'init',                               array( $this, 'register_post_type' ) );
+		add_filter( 'post_type_link',                     array( $this, 'post_type_link' ), 10, 2 );
+		add_filter( 'pre_get_posts',                      array( $this, 'pre_get_posts' ) );
+		add_action( 'widgets_init',                       array( $this, 'handbook_sidebar' ), 11 ); // After P2
+		add_action( 'wporg_email_changes_for_post_types', array( $this, 'wporg_email_changes_for_post_types' ) );
+		add_action( 'p2_action_links',                    array( $this, 'disable_p2_resolved_posts_action_links' ) );
+		add_action( 'admin_init',                         array( $this, 'add_name_setting' ) );
+	}
+
+	function add_name_setting() {
+		register_setting( 'general', $this->setting_name, 'esc_attr' );
+
+		$label = ( 'handbook' == $this->post_type ) ?
+			__( 'Handbook name', 'wporg' ) :
+			sprintf( __( 'Handbook name (%s)', 'wporg' ), substr( $this->post_type, 0, -9 ) );
+
+		add_settings_field(
+			$this->setting_name,
+			'<label for="' . esc_attr( $this->setting_name ) . '">' . $label . '</label>',
+			array( $this, 'name_setting_html' ),
+			'general'
+		);
+	}
+
+	function name_setting_html() {
+		$value = get_option( $this->setting_name, '' );
+		echo '<input type="text" id="' . esc_attr( $this->setting_name ) . '" name="' . esc_attr( $this->setting_name ) . '" value="' . esc_attr( $value ) . '" class="regular-text ltr" />';
+	}
+
+	function grant_handbook_caps( $caps ) {
+		if ( ! is_user_member_of_blog() ) {
+			return $caps;
+		}
+
+		foreach ( self::caps() as $cap ) {
+			$caps[ $cap ] = true;
+		}
+
+		if ( ! empty( $caps['edit_pages'] ) ) {
+			foreach ( self::editor_caps() as $cap ) {
+				$caps[ $cap ] = true;
+			}
+		}
+
+		return $caps;
+	}
+
+	function register_post_type() {
+		if ( 'handbook' != $this->post_type ) {
+			$slug = substr( $this->post_type, 0, -9 );
+		} else {
+			$slug = 'handbook';
+		}
+
+		$default_config = array(
+			'labels' => array(
+				'name'          => $this->label,
+				'singular_name' => sprintf( __( '%s Page', 'wporg' ), $this->label ),
+				'menu_name'     => $this->label,
+				'all_items'     => sprintf( __( '%s Pages', 'wporg' ), $this->label ),
+			),
+			'public'            => true,
+			'show_ui'           => true,
+			'capability_type'   => 'handbook_page',
+			'map_meta_cap'      => true,
+			'has_archive'       => true,
+			'hierarchical'      => true,
+			'menu_position'     => 11,
+			'rewrite' => array(
+				'feeds'         => false,
+				'slug'          => $slug,
+				'with_front'    => false,
+			),
+			'delete_with_user'  => false,
+			'supports'          => array( 'title', 'editor', 'author', 'thumbnail', 'page-attributes', 'custom-fields', 'comments', 'revisions' ),
+		);
+		// Allow customization of the default post type configuration via filter.
+		$config = apply_filters( 'handbook_post_type_defaults', $default_config, $slug );
+
+		register_post_type( $this->post_type, $config );
+	}
+
+	function post_type_link( $link, $post ) {
+		if ( $post->post_type === $this->post_type && $post->post_name === $this->post_type ) {
+			return get_post_type_archive_link( $this->post_type );
+		}
+
+		return $link;
+	}
+
+	function pre_get_posts( $query ) {
+		if ( $query->is_main_query() && ! $query->is_admin && $query->is_post_type_archive( $this->post_type ) ) {
+			// If the post type has a page to act as an archive index page, get that.
+			if ( $page = get_page_by_path( $this->post_type, OBJECT, $this->post_type ) ) {
+				$query->set( 'p', $page->ID );
+			}
+			$query->set( 'handbook', $this->post_type );
+		}
+	}
+
+	function handbook_sidebar() {
+		register_sidebar( array(
+			'id'          => $this->post_type,
+			'name'        => $this->label,
+			'description' => sprintf( __( 'Used on %s pages', 'wporg' ), $this->label ),
+		) );
+		require_once dirname( __FILE__ ) . '/inc/widgets.php';
+		register_widget( 'WPorg_Handbook_Pages_Widget' );
+	}
+
+	function wporg_email_changes_for_post_types( $post_types ) {
+		if ( ! in_array( $this->post_type, $post_types ) ) {
+			$post_types[] = $this->post_type;
+		}
+
+		return $post_types;
+	}
+
+	/**
+	 * Disable the P2 Resolved Posts plugin's action links (e.g. "Flag Unresolved"),
+	 * if that plugin is active.
+	 */
+	function disable_p2_resolved_posts_action_links() {
+		if ( ( $this->post_type == get_post_type() ) && class_exists( 'P2_Resolved_Posts' ) && is_object( P2_Resolved_Posts::$instance ) ) {
+			remove_filter( 'p2_action_links', array( P2_Resolved_Posts::$instance, 'p2_action_links' ), 100 );
+		}
+	}
+
+}
---
+++ wp-content/plugins/handbook/inc/table-of-contents.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Adds a table of contents to your pages based on h1, h2, h3 and h4 tags. Useful for documention-centric sites.
+ *
+ * @author Automattic, modifed by Nacin
+ */
+class WPorg_Handbook_TOC {
+	protected $post_types = array();
+
+	protected $styles = '<style> .toc-jump { text-align: right; font-size: 12px; } .page .toc-heading { margin-top: -50px; padding-top: 50px !important; }</style>';
+
+	function __construct( $post_types ) {
+		$this->post_types = (array) $post_types;
+		add_action( 'template_redirect', array( $this, 'load_filters' ) );
+	}
+
+	function load_filters() {
+		$this->post_types = array_map( array( $this, 'append_suffix' ), $this->post_types );
+
+		if ( is_singular( $this->post_types ) )
+			add_filter( 'the_content', array( $this, 'add_toc' ) );
+	}
+
+	function append_suffix( $t ) {
+		if ( in_array( $t, array( 'handbook', 'page' ) ) ) {
+			return $t;
+		}
+
+		return $t . '-handbook';
+	}
+
+	function add_toc( $content ) {
+		$toc = '';
+
+		$items = $this->get_tags( 'h([1-4])', $content );
+
+		for ( $i = 1; $i <= 4; $i++ )
+			$content = $this->add_ids_and_jumpto_links( "h$i", $content );
+
+		if ( $items ) {
+			$contents_header = 'h' . $items[0][2]; // Duplicate the first <h#> tag in the document.
+			$toc .= $this->styles;
+			$toc .= '<div class="table-of-contents">';
+			$toc .= "<$contents_header>" . __( 'Topics', 'wporg' ) . "</$contents_header><ul class=\"items\">";
+			$last_item = false;
+			foreach ( $items as $item ) {
+				if ( $last_item ) {
+					if ( $last_item < $item[2] )
+						$toc .= "\n<ul>\n";
+					elseif ( $last_item > $item[2] )
+						$toc .= "\n</ul></li>\n";
+					else
+						$toc .= "</li>\n";
+				}
+
+				$last_item = $item[2];
+				$toc .= '<li><a href="#' . sanitize_title_with_dashes($item[3])  . '">' . $item[3]  . '</a>';
+			}
+			$toc .= "</ul>\n</div>\n";
+		}
+
+		return $toc . $content;
+	}
+
+	protected function add_ids_and_jumpto_links( $tag, $content ) {
+		$items = $this->get_tags( $tag, $content );
+		$first = true;
+		$matches = array();
+		$replacements = array();
+
+		foreach ( $items as $item ) {
+			$replacement = '';
+			$matches[] = $item[0];
+			$id = sanitize_title_with_dashes($item[2]);
+
+			if ( ! $first ) {
+				$replacement .= '<p class="toc-jump"><a href="#top">' . __( 'Top &uarr;', 'wporg' ) . '</a></p>';
+			} else {
+				$first = false;
+			}
+
+			$replacement .= sprintf( '<%1$s class="toc-heading" id="%2$s" tabindex="-1">%3$s <a href="#%2$s" class="anchor">#</a></%1$s>', $tag, $id, $item[2] );
+			$replacements[] = $replacement;
+		}
+
+		if ( $replacements ) {
+			$content = str_replace( $matches, $replacements, $content );
+		}
+
+		return $content;
+	}
+
+	private function get_tags( $tag, $content = '' ) {
+		if ( empty( $content ) )
+			$content = get_the_content();
+		preg_match_all( "/(<{$tag}>)(.*)(<\/{$tag}>)/", $content, $matches, PREG_SET_ORDER );
+		return $matches;
+	}
+}
+
