7429d1b6eb32eef0b34383642cd4ee7d429c1c76
Pea <pea@misfist.com>
2015-10-15 21:30:53 -0400

    Cleaned up single view.

--- wp-content/plugins/glocal-meetings/views/content-single.php
+++ wp-content/plugins/glocal-meetings/views/content-single.php
@@ -5,8 +5,6 @@
  * Variables that can be used in the template
  */
 
-$meeting_agenda = wpautop( get_post_meta($post->ID, 'meeting_agenda', true), true );
-$meeting_links = get_post_meta($post->ID, 'meeting_links', true);
 $meeting_date = date_i18n( get_option( 'date_format' ), strtotime( get_post_meta($post->ID, 'meeting_date', true) ) );
 $meeting_type = get_the_term_list( $post->ID, 'anp_meetings_type', '<span class="category">', ', ', '</span>' );
 $meeting_tags = get_the_term_list( $post->ID, 'anp_meetings_tag', '<span class="tags">', ', ', '</span>' );
@@ -20,8 +18,8 @@ $meeting_pre_content = '';
 
 $meeting_pre_content .= '<div class="pre-content">';
 
-$meeting_pre_content .= '<p class="meta"><span class="meta-label">' . __( 'Date:', 'anp_meetings' ) . '</span> ' . $meeting_date . '</p>';
-$meeting_pre_content .= '<p class="meta"><span class="meta-label">' . __( 'Type:', 'anp_meetings' ) . '</span> ' . $meeting_type . '</p>';
+// $meeting_pre_content .= '<p class="meta"><span class="meta-label">' . __( 'Date:', 'anp_meetings' ) . '</span> ' . $meeting_date . '</p>';
+// $meeting_pre_content .= '<p class="meta"><span class="meta-label">' . __( 'Type:', 'anp_meetings' ) . '</span> ' . $meeting_type . '</p>';
 
 if( !empty($meeting_agenda) ) {
 	$meeting_pre_content .= '<div class="meeting-agenda">';
@@ -41,21 +39,6 @@ $meeting_pre_content .= '<h2 id="meeting-notes">' . __( 'Notes', 'anp_meetings' 
 
 $meeting_post_content = '';
 $meeting_post_content .= '<footer class="post-footer">';
-
-if( !empty($meeting_links) ) {
-	$meeting_post_content .= '<h3 id="meeting-links">Associated Content</h3>';
-	$meeting_post_content .= '<ul class="meeting-links">';
-
-	foreach($meeting_links as $link) {
-		$meeting_post_content .= '<li>';
-		$meeting_post_content .= '<a href="' . get_permalink( $link ) . '">';
-		$meeting_post_content .= get_the_title( $link );
-		$meeting_post_content .= '</a>';
-		$meeting_post_content .= '</li>';
-	}
-
-	$meeting_post_content .= '</ul>';
-}
 
 $meeting_post_content .= '<p class="tags meta"><span class="meta-label">' . __( 'Tags:', 'anp_meetings' ) . '</span> ';
 $meeting_post_content .= $meeting_tags;
