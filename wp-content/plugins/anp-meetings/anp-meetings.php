5e700791fb6223de427656c7aeced0b8466990c6
Pea <pea@misfist.com>
2015-10-19 19:50:53 -0400

    Moved custom fields to separate file.

--- wp-content/plugins/anp-meetings/anp-meetings.php
+++ wp-content/plugins/anp-meetings/anp-meetings.php
@@ -1,4 +1,15 @@
 <?php
+
+/**
+ * ANP Meetings Init
+ *
+ * @author    Pea, Glocal
+ * @license   GPL-2.0+
+ * @link      http://glocal.coop
+ * @since     1.0.0
+ * @package   ANP_Meetings
+ */
+
 /*
 Plugin Name: Activist Network Meetings
 Description: Creates custom post types for Meetings with custom fields and custom taxonomies that can be used to store and display meeting notes/minutes and decisions.
@@ -19,6 +30,7 @@ include_once('inc/custom-post-type-meetings.php');
 include_once('inc/custom-post-type-agendas.php');
 include_once('inc/custom-post-type-summaries.php');
 include_once('inc/custom-post-type-proposals.php');
+include_once('inc/custom-fields.php');
 include_once('inc/post-type-connections.php');
 
 
---
+++ wp-content/plugins/anp-meetings/inc/custom-fields.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * ANP Meetings Summaries Post Type
+ *
+ * @author    Pea, Glocal
+ * @license   GPL-2.0+
+ * @link      http://glocal.coop
+ * @since     1.0.0
+ * @package   ANP_Meetings
+ */
+
+
+/************* CUSTOM FIELDS *****************/
+
+if( function_exists( "register_field_group" ) ) {
+
+    register_field_group( array(
+        'id' => 'acf_meeting-date',
+        'title' => 'Meeting Date',
+        'fields' => array(
+            array(
+                'key' => 'field_56257bc5c5ea1',
+                'label' => 'Meeting Date',
+                'name' => 'meeting_date',
+                'type' => 'date_picker',
+                'required' => 1,
+                'date_format' => 'yymmdd',
+                'display_format' => 'mm/dd/yy',
+                'first_day' => 1,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'anp_meetings',
+                    'order_no' => 0,
+                    'group_no' => 0,
+                ),
+            ),
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'anp_agenda',
+                    'order_no' => 0,
+                    'group_no' => 1,
+                ),
+            ),
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'anp_summary',
+                    'order_no' => 0,
+                    'group_no' => 2,
+                ),
+            ),
+        ),
+        'options' => array(
+            'position' => 'acf_after_title',
+            'layout' => 'no_box',
+            'hide_on_screen' => array(
+            ),
+        ),
+        'menu_order' => 0,
+    ));
+
+
+
+    register_field_group( array(
+        'id' => 'acf_proposal-detail-page',
+        'title' => 'Proposal Detail Page',
+        'fields' => array(
+            array(
+                'key' => 'field_562008d369f6b',
+                'label' => 'Status',
+                'name' => 'proposal_status',
+                'type' => 'taxonomy',
+                'taxonomy' => 'anp_proposal_status',
+                'field_type' => 'select',
+                'allow_null' => 1,
+                'load_save_terms' => 1,
+                'return_format' => 'id',
+                'multiple' => 0,
+            ),
+            array(
+                'key' => 'field_562008263445d',
+                'label' => 'Date Accepted',
+                'name' => 'meeting_date',
+                'type' => 'date_picker',
+                'date_format' => 'yymmdd',
+                'display_format' => 'mm/dd/yy',
+                'first_day' => 1,
+            ),
+            array(
+                'key' => 'field_5620088b2365a',
+                'label' => 'Date Effective',
+                'name' => 'proposal_date_effective',
+                'type' => 'date_picker',
+                'date_format' => 'yymmdd',
+                'display_format' => 'mm/dd/yy',
+                'first_day' => 1,
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'anp_proposal',
+                    'order_no' => 0,
+                    'group_no' => 0,
+                ),
+            ),
+        ),
+        'options' => array(
+            'position' => 'acf_after_title',
+            'layout' => 'default',
+            'hide_on_screen' => array(
+                0 => 'custom_fields',
+                1 => 'featured_image',
+            ),
+        ),
+        'menu_order' => 0,
+    ));
+
+}
+
+?>
