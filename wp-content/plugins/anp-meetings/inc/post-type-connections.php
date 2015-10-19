7bf51e54c2f3861341d9a46c57ea414169fffd8c
Pea <pea@misfist.com>
2015-10-19 19:51:41 -0400

    Updated Posts to Posts connection types

--- wp-content/plugins/anp-meetings/inc/post-type-connections.php
+++ wp-content/plugins/anp-meetings/inc/post-type-connections.php
@@ -1,22 +1,50 @@
 <?php 
 
+/**
+ * ANP Meetings Posts 2 Posts Connections
+ *
+ * @author    Pea, Glocal
+ * @license   GPL-2.0+
+ * @link      http://glocal.coop
+ * @since     1.0.0
+ * @package   ANP_Meetings
+ */
+
 function anp_meetings_connection_types() {
     p2p_register_connection_type( array(
         'name' => 'meeting_to_agenda',
         'from' => 'anp_meetings',
-        'to' => 'anp_agenda'
+        'to' => 'anp_agenda',
+        'reciprocal' => true,
+        'cardinality' => 'one-to-one',
+        'admin_column' => true,
+        'admin_dropdown' => 'to',
+        'sortable' => 'any',
+        // 'title' => __( 'Agenda', 'anp_meetings' ),
     ) );
 
     p2p_register_connection_type( array(
         'name' => 'meeting_to_summary',
         'from' => 'anp_meetings',
-        'to' => 'anp_summary'
+        'to' => 'anp_summary',
+        'reciprocal' => true,
+        'cardinality' => 'one-to-one',
+        'admin_column' => true,
+        'admin_dropdown' => 'to',
+        'sortable' => 'any',
+        // 'title' => __( 'Summary', 'anp_meetings' ),
     ) );
 
     p2p_register_connection_type( array(
         'name' => 'meeting_to_proposals',
         'from' => 'anp_meetings',
-        'to' => 'anp_proposal'
+        'to' => 'anp_proposal',
+        'reciprocal' => true,
+        'cardinality' => 'one-to-many',
+        'admin_column' => true,
+        'admin_dropdown' => 'any',
+        'sortable' => 'any',
+        // 'title' => __( 'Proposals', 'anp_meetings' ),
     ) );
 
 }
