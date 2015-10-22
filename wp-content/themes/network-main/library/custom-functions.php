6e00d7933dd3eec195a69532a2ff2a38c062b545
Pea <pea@misfist.com>
2015-10-22 19:59:14 -0400

    Added conditional to account for archive pages.

--- wp-content/themes/network-main/library/custom-functions.php
+++ wp-content/themes/network-main/library/custom-functions.php
@@ -82,10 +82,10 @@ add_action( 'manage_pages_custom_column', 'glocal_display_post_thumbnail_column'
 
 // Add specific CSS class by filter
 add_filter('body_class','glocal_class_names');
-function glocal_class_names($classes) {
+function glocal_class_names( $classes ) {
 	// add 'class-name' to the $classes array
 	global $post; 
-	$post_slug_class = $post->post_name; 
+	$post_slug_class = ( 1 == count( $post ) ) ? $post->post_name : ''; 
 	$classes[] = $post_slug_class . ' page-' . $post_slug_class;
 	// return the $classes array
 	return $classes;
