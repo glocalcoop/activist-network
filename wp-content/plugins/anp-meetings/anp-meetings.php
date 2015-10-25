16228c38dc5100867022f8fd73fc309b4871bb30
Pea <pea@misfist.com>
2015-10-25 13:17:17 -0400

    Added constant for plugin root.

--- wp-content/plugins/anp-meetings/anp-meetings.php
+++ wp-content/plugins/anp-meetings/anp-meetings.php
@@ -25,18 +25,35 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-/************* REQUIRE FILES *****************/
+
+/* ---------------------------------- *
+ * Constants
+ * ---------------------------------- */
+
+if ( !defined( 'ANP_MEETINGS_PLUGIN_DIR' ) ) {
+    define( 'ANP_MEETINGS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( !defined( 'ANP_MEETINGS_PLUGIN_URL' ) ) {
+    define( 'ANP_MEETINGS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/* ---------------------------------- *
+ * Required Files
+ * ---------------------------------- */
 
 //define( 'ACF_LITE', true );
-include_once('advanced-custom-fields/acf.php');
-include_once('posts-to-posts/posts-to-posts.php');
-include_once('anp-meetings-render.php');
-include_once('inc/custom-post-type-meetings.php');
-include_once('inc/custom-post-type-agendas.php');
-include_once('inc/custom-post-type-summaries.php');
-include_once('inc/custom-post-type-proposals.php');
-include_once('inc/custom-fields.php');
-include_once('inc/post-type-connections.php');
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'advanced-custom-fields/acf.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'posts-to-posts/posts-to-posts.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'anp-meetings-render.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-content-filters.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-pre-get-filters.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-post-type-meetings.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-post-type-agendas.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-post-type-summaries.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-post-type-proposals.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/custom-fields.php' );
+include_once( ANP_MEETINGS_PLUGIN_DIR . 'inc/post-type-connections.php' );
 
 
 
