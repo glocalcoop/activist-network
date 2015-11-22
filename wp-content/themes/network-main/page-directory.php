36e5e86c04a3959ecded661aab152f988c13dbea
Pea <pea@misfist.com>
2015-11-21 19:20:20 -0500

    Fixed  Notice: Trying to get property of non-object in /home/members/glocalcoop/sites/activistnetwork.dev.glocal.coop/web/wp-content/themes/network-main/page-directory.php on line 81

--- wp-content/themes/network-main/page-directory.php
+++ wp-content/themes/network-main/page-directory.php
@@ -56,35 +56,17 @@ Template Name: Network Sites
 							// Get post count for each site
 							$site_details = get_blog_details($site_id);
 
-							// Find Network pages that are associated with this site
-							$args = array (
-								'post_type'         => 'network',
-								'meta_query'        => array(
-									array(
-										'key'       => 'glocal_network_sites',
-										'value'     => $site_id,
-										'compare'   => '=',
-									),
-								),
-							);
-							$network_query = new WP_Query( $args );
-
 							?>
 							<?php
 							if(function_exists('glocal_get_site_image')) {
 								$header = glocal_get_site_image($site_id);
 							} ?>
 
-							<li class="isomote id-<?php echo $site_id; ?> site-<?php echo $site_slug; ?> network-<?php foreach($network_query as $post){ echo $post->post_name;} ?>" data-id="<?php echo $site_id ?>" data-slug="<?php echo $site_slug ?>" data-posts="<?php echo $site_details->post_count; ?>">
-								<a href="<?php echo $site_details->siteurl; ?>" class="item-image <?php if(!$header) { echo 'no-image'; } ?>" style="background-image: url('<?php if($header) { echo $header; } ?>');"></a>
+							<li class="isomote id-<?php echo $site_id; ?> site-<?php echo $site_slug; ?>" data-id="<?php echo $site_id ?>" data-slug="<?php echo $site_slug ?>" data-posts="<?php echo $site_details->post_count; ?>">
+								<a href="<?php echo $site_details->siteurl; ?>" class="item-image<?php echo (! $header ) ? ' no-image' : ''; ?>" style="background-image: url('<?php echo ( $header ) ? $header : ''; ?>');"></a>
 								<h3 class="item-title"><a href="<?php echo $site_details->siteurl; ?>"><?php echo $site_details->blogname; ?></a></h3>
-								<h6 class="meta item-network"><?php foreach($network_query as $post){ echo $post->post_title;} ?></h6>
 								<h6 class="meta item-posts">
-								<?php
-									if($site_details->post_count) {
-										echo $site_details->post_count . ' posts';
-									}
-								?>
+								<?php echo ( $site_details->post_count > 0 ) ? $site_details->post_count . ' posts' : ''; ?>
 								</h6>
 								<h6 class="meta item-topic"></h6>
 							</li>
