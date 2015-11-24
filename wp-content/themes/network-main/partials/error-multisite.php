ebbbbe06afe46be9a08de32e22fab1bec05f1dc5
Pea <pea@misfist.com>
2015-11-24 14:39:18 -0500

    Remove custom themes in order to add as submodules.

--- wp-content/themes/network-main/partials/error-multisite.php
+++
@@ -1,2 +0,0 @@
-<h2 class="module-heading">Sorry</h2>
-<p><?php _e( 'This theme is designed for WP multisite installations. Please activate multisite or try a different theme.', 'glocal-theme' ); ?></p>
--- wp-content/themes/network-main/partials/error-plugin.php
+++
@@ -1,2 +0,0 @@
-<h2 class="module-heading">Sorry</h2>
-<p><?php _e( 'This theme requires a plugin to display this content.', 'glocal-theme' ); ?></p>
