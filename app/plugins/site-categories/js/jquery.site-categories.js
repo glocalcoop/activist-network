a092d1e55932e979e67504d38e472f21a6712b17
Pea <pea@misfist.com>
2015-11-24 17:46:54 -0500

    Remove dev and other plugins from repo.

--- app/plugins/site-categories/js/jquery.site-categories.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($) {
-	jQuery( "#site-categories-wrapper .site-categories-accordion" ).accordion({ heightStyle: 'content', header: '.site-categories-accordion-header' });
-});
