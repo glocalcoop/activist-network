11e3e6de62aed511d58bd17501be1fdfb5f895c1
Dana Skallman <dana@glocal.coop>
2015-11-30 16:28:03 -0500

    Plugin Update - Custom Contact Forms

--- app/plugins/custom-contact-forms/classes/class-ccf-ads.php
+++ app/plugins/custom-contact-forms/classes/class-ccf-ads.php
@@ -3,67 +3,87 @@
 class CCF_Ads {
 
 	/**
-	 * Setup ads
+	 * Placeholder method
 	 *
-	 * @since  6.9.1
+	 * @since 6.9.4
+	 */
+	public function __construct() {}
+
+	/**
+	 * Setup hooks
+	 *
+	 * @since 6.9.4
 	 */
 	public function setup() {
-		add_action( 'admin_notices', array( $this, 'ad_nag' ) );
-		add_action( 'init', array( $this, 'hide_ads' ) );
+		add_action( 'admin_notices', array( $this, 'show_ad' ) );
+		add_action( 'init', array( $this, 'process_submission' ) );
 	}
 
-	public function hide_ads() {
-		if ( ! empty( $_POST['ccf_hide_ads'] ) ) {
-			update_option( 'ccf_hide_ads', true );
-		} elseif ( ! empty( $_POST['ccf_hosting_signup'] ) && ! empty( $_POST['email'] ) ) {
+	public function process_submission() {
+		if ( ! empty( $_POST['ccf_subscribe'] ) && ! empty( $_POST['email'] ) ) {
+			$request = wp_remote_request( 'http://taylorlovett.us8.list-manage.com/subscribe/post?u=66118f9a5b0ab0414e83f043a&amp;id=b4ed816a24', array(
+				'method' => 'post',
+				'body' => array(
+					'EMAIL' => $_POST['email'],
+				)
+			));
 
-			$headers = 'From: CCF User <' . $_POST['email'] . '>' . "\r\n";
-			wp_mail( 'tlovett88@gmail.com', 'I Want WordPress Hosting on http://' . $_SERVER['HTTP_HOST'], 'Hey! I\'d love some info on WordPress hosting. My email is ' . $_POST['email'] . ' and my website is http://' . $_SERVER['HTTP_HOST'] . '.', $headers );
-
-			update_option( 'ccf_hide_ads', true );
+			update_option( 'ccf_subscribed', 1 );
 		}
 	}
 
 	/**
-	 * Show ads
+	 * Output ad
 	 *
-	 * @since 6.9.1
+	 * @since 6.9.4
 	 */
-	public function ad_nag() {
-		$hide_ads = get_option( 'ccf_hide_ads', false);
+	public function show_ad() {
+		global $pagenow;
 
-		if ( $hide_ads ) {
+		if ( 'edit.php' === $pagenow || 'post-new.php' === $pagenow ) {
+			if ( empty( $_GET['post_type'] ) || 'ccf_form' !== $_GET['post_type'] ) {
+				return;
+			}
+		}
+
+		if ( 'post.php' === $pagenow ) {
+			if ( 'ccf_form' !== get_post_type() ) {
+				return;
+			}
+		}
+
+		if ( 'post.php' !== $pagenow && 'edit.php' !== $pagenow && 'post-new.php' !== $pagenow ) {
 			return;
 		}
+
+		$subscribed = get_option( 'ccf_subscribed' );
+
+		if ( ! empty( $subscribed ) ) {
+			return;
+		}
+		
 		?>
-
-		<form method="post" action="" class="update-nag ccf-hosting-offer">
-
-			<h3>Is Your WordPress Site Running Slowly</h3>
-
-			<p>Sign up for affordable no hassle, managed WordPress hosting including:</p>
-
-			<ul>
-				<li>Daily backups taken automatically.</li>
-				<li>High speed caching.</li>
-				<li>Dedicated hosting. You won't share space with any other websites.</li>
-				<li>Optimized server settings for WordPress.</li>
-				<li>Free support 9-5pm EDT Monday - Friday. Emergency support 24/7.</li>
-				<li>100% uptime.</li>
-				<li>Latest versions of PHP, MySQL, and WordPress.
-				<li>Free migration to new platform.</li>
-			</ul>
-
-			<p><input name="email" type="text" placeholder="Email address" /> <input name="ccf_hosting_signup" class="button button-primary" type="submit" value="Get more info" /> <input name="ccf_hide_ads" class="button" type="submit" value="No thanks" /></p>
-		</form>
-
+		<div class="updated update-nag ccf-subscribe">
+			<form method="post">
+				<p>
+					<?php if ( empty( $_POST['ccf_subscribe'] ) || empty( $_POST['email'] ) ) : ?>
+						WordPress exclusive tutorials, blogging tips, plugins, and more. 
+						<input type="email" name="email">
+						<input type="hidden" name="ccf_subscribe" value="1">
+						<input type="submit" class="button button-primary" value="Sign Me Up">
+					<?php else : ?>
+						Check your email to confirm your subscription!
+					<?php endif; ?>
+				</p>
+			</form>
+		</div>
 		<?php
 	}
 
 	/**
 	 * Return singleton instance of class
 	 *
-	 * @since 6.1
+	 * @since 6.9.4
 	 * @return object
 	 */
 	public static function factory() {
